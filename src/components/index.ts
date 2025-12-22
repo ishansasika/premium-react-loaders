4ba19379de9577062b451ba9209fa932eb523495
ishansasika <ishansasika@gmail.com>
2025-12-22 14:10:52 +0530

    feat: Add main entry point and component exports

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+// Skeleton components
+export * from './skeleton';
+
+// Spinner components
+export * from './spinner';
+
+// Progress components
+export * from './progress';
+
+// Pulse components
+export * from './pulse';
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Import styles
+import './styles/index.css';
+
+// Component exports
+export * from './components';
+
+// Type exports
+export type * from './types';
+
+// Utility exports
+export { cn } from './utils/classNames';
+export { getAnimationDuration, normalizeSize } from './utils/colors';
+
+// Version
+export const version = '1.0.0';
