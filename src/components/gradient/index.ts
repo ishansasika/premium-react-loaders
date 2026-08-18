f06a3e6adcd71d09d5a56bc97daef036f4a674a9
ishansasika <ishansasika@gmail.com>
2026-08-18 22:45:03 +0530

    feat: Export GradientText and GradientDots from Gradient category

--- src/components/gradient/index.ts
+++ src/components/gradient/index.ts
@@ -1,3 +1,5 @@
 export { GradientSpinner } from './GradientSpinner';
 export { GradientRing } from './GradientRing';
 export { GradientBar } from './GradientBar';
+export { GradientText } from './GradientText';
+export { GradientDots } from './GradientDots';
